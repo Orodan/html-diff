6542d3e9eb66f7329c80ef456f27d5915ae39479
Orodan <jimmykasprzak@gmail.com>
2021-03-22 23:50:25 +0100

    ✨ override generic wrapper

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const targetElement = document.getElementById('destination-elem-id');
-const configuration = { };
+const configuration = { outputFormat: 'side-by-side', rawTemplates: { 'generic-wrapper': '<div class="my-custom-wrapper"><h1>Welcome</h1>{{{ content }}}</div>' } };
 const diffString = `diff --git a/sample.js b/sample.js
 index 0000001..0ddf2ba
 --- a/sample.js
@@ -11,5 +11,7 @@ index 0000001..0ddf2ba
 const diff2htmlUi = new Diff2HtmlUI(targetElement, diffString, configuration);
 // or
 // const diff2htmlUi = new Diff2HtmlUI(targetElement, diffJson, configuration);
+
+console.log('diff2htmlUi: ', diff2htmlUi)
 
 diff2htmlUi.draw();
