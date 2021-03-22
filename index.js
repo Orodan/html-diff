9bd1272c6dc2d4804f7f7903ad4e5689a895aac8
Orodan <jimmykasprzak@gmail.com>
2021-03-23 00:17:48 +0100

    ✨ setup of demo using html

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const targetElement = document.getElementById('destination-elem-id');
-const configuration = { outputFormat: 'side-by-side', rawTemplates: { 'generic-wrapper': '<div class="my-custom-wrapper"><h1>Welcome</h1>{{{ content }}}</div>' } };
-const diffString = `diff --git a/sample.js b/sample.js
+// Interpredted html in content variable ?
+const customGenericLineTemplate = `
+    <tr>
+        <td class="{{lineClass}} {{type}}">
+        {{{lineNumber}}}
+        </td>
+        <td class="{{type}}">
+            <div class="{{contentClass}} {{type}}">
+                <span class="d2h-code-line-ctn">{{{ content }}}</span>
+            </div>
+        </td>
+    </tr>
+`
+const configuration = { outputFormat: 'side-by-side', rawTemplates: { 
+    'generic-wrapper': '<div class="my-custom-wrapper"><h1>Welcome</h1>{{{ content }}}</div>',
+    'generic-line': customGenericLineTemplate
+} };
+const diffString = `diff --git a/sample.html b/sample.html
 index 0000001..0ddf2ba
---- a/sample.js
-+++ b/sample.js
+--- a/sample.html
++++ b/sample.html
 @@ -1 +1 @@
--console.log("Hello World!")
-+console.log("Hello from Diff2Html!")`;
+-<h1>Hello World!</h1>
++<h1>Hello from Diff2Html!</h1>`;
 
 const diff2htmlUi = new Diff2HtmlUI(targetElement, diffString, configuration);
-// or
-// const diff2htmlUi = new Diff2HtmlUI(targetElement, diffJson, configuration);
 
 console.log('diff2htmlUi: ', diff2htmlUi)
 
